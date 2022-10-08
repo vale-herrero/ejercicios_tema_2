84b68db1390a9773d9c2979c9298f96dfd8ac13c
vale-herrero <vale4134@gmail.com>
2022-10-08 16:20:01 +0200

    terminé ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,15 @@
+
 texto= "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 def transformar(parrafo):
-    parrafo= parrafo.replace("#","\n- ")
-    parrafo=parrafo.capitalize()
+    parrafo= parrafo.replace("#","\n")
+    lineas = parrafo.split("\n")
+    for posicion in range(len(lineas)):
+        if posicion==0:
+            lineas[posicion]= lineas[posicion].capitalize()+"..."
+        else:
+            lineas[posicion]= "- "+lineas[posicion].capitalize()+"."
+    parrafo="\n".join(lineas)
     return parrafo
+
 
 print(transformar(texto))
